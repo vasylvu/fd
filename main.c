9efc887ae0d14c186bd84a44f130e2deb43ec9af
vasyl.yushchyshen <vasilyvu@gmail.com>
2014-10-09 23:13:17 +0300

    Used two-axis file list
    Signed-off-by: vasyl.yushchyshen <vasilyvu@gmail.com>

--- main.c
+++ main.c
@@ -11,23 +11,20 @@
 
 #define FULLNAME_LEN 1024
 
+#define ERR_NOMEM		-1
+#define ERR_READ_DIR	-4
+#define ERR_MEM_MAP		-5
+#define ERR_OPEN_FILE	-6
+
+
 struct list_entry {
 	struct list_entry *next;
+	struct list_entry *next_down;	/* List entry for more than two files of the same size	*/
 	unsigned long size;
-	char	name[FULLNAME_LEN];
-};
-
-/* List entry for more than two files of the same size
-*
-* For this case first calculate CRC of each file part,
-* then the files is compared if CRC is equal.
-*/
-struct crc_entry {
-	struct crc_entry *next;
-	char *name;
-	unsigned short crc;
 	int fd;
 	void *mm;
+	unsigned short crc;
+	char name[FULLNAME_LEN];
 };
 
 static int files_scanned_num, files_failed_num;
@@ -69,13 +66,13 @@ int	scan_dir(char *dir_name, unsigned long min_size, int(*list_cb)(struct list_e
 	DIR * dir;
 	struct dirent *entry;
 	char full_name[FULLNAME_LEN];
-	int size;
+	int size, ret;
 	
 	dir = opendir(dir_name);
 
 	if (dir == NULL) {
 		printf("Error reading of directory %s\n", dir_name);
-		return -1;
+		return ERR_READ_DIR;
 	}
 
 	while (NULL != (entry = readdir(dir))) {
@@ -85,82 +82,91 @@ int	scan_dir(char *dir_name, unsigned long min_size, int(*list_cb)(struct list_e
 		strcpy(full_name, dir_name);
 		strcat(full_name, "/");
 		strcat(full_name, entry->d_name);
-		if(entry->d_type == 0x04) {
-			if(scan_dir(full_name, min_size, list_cb, list)) {
+		if(entry->d_type == DT_DIR) {
+			if(ret = scan_dir(full_name, min_size, list_cb, list)) {
 				closedir(dir);
-				return -3;		/* TODO: error code  */
+				return ret;
 			}
-		} else {
+		} else if(entry->d_type == DT_REG) {
 			size = file_size(full_name);
 			if (size >= min_size)
 				if ((*list_cb)(list, full_name, size)) {
 					closedir(dir);
-					printf("Error list entry inserting\n");
 					inc_files_failed(); 
-					return -2;
+					return ERR_NOMEM;
 				}
 		}
 	}
 	closedir(dir);
-
 	return 0;
 }
 
-/* Inserting record into file list arranged by size */
+/* Inserting record into the file list arranged by size */
 int insert_file_entry(struct list_entry *list, char *name, unsigned long size)
 {
 	struct list_entry *n; /* new entry */
 	n = malloc(strlen(name) + sizeof(struct list_entry) - FULLNAME_LEN + 1);
-	if(NULL == n)
-		return 1;
-	strcpy(&(n->name[0]), name);
+	if(NULL == n) {
+		printf("Insufficient of memory\n");
+		return ERR_NOMEM;
+	}
+	strcpy(n->name, name);
 	n->size = size;
 
-	while(list != NULL){
-		if (list->next != NULL && size >= list->size && size <= list->next->size) {
-			n->next = list->next;
-			list->next = n;
-			break;
+	for(; list != NULL; list = list->next){
+		if (list->next != NULL) {
+			if (size < list->size) {		// TODO: check if need
+				printf("-- find next size %lu\n", size);
+				continue;
+			}
+			else if (size > list->size && size < list->next->size) {
+				n->next = list->next;
+				n->next_down = NULL;
+				list->next = n;
+				break;
+			}
+			else if (size == list->size) {
+				n->next = NULL;
+				n->next_down = list->next_down;
+				list->next_down = n;
+				break;
+			}
 		}
-		else if(list->next == NULL) {
+		else {
 			n->next = NULL;
+			n->next_down = NULL;
 			list->next = n;
 			break;
 		}
-			list = list->next;
 	}
-	
 	inc_files_scanned();
 	return 0;
 }
 
 void clean_malloc(struct list_entry *list)
 {
-	struct list_entry *curr_ptr;
+	struct list_entry *tmp, *tmpd;
 
 	while (list != NULL) {
-		curr_ptr = list;
+		tmpd = list->next_down;
+		while(NULL != tmpd) {			
+			tmp = tmpd;
+			tmpd = tmpd->next_down;
+			free(tmp);
+		}
+		tmp = list;
 		list = list->next;
-		free(curr_ptr);
+		free(tmp);
 	}
 }
 
-void unmap_and_close(struct crc_entry *list, int size)
+void unmap_and_close(struct list_entry *list)
 {
-	while (list != NULL) {
+	for (; list != NULL; list = list->next_down) {
 		if (NULL != list->mm)
-			munmap(list->mm, size);
+			munmap(list->mm, list->size);
 		if (0 != list->fd)
 			close(list->fd);
-		list = list->next;
-	}
-}
-
-void show_list(struct list_entry *list)
-{
-	while(list != NULL) {
-		printf("size=%lu %s\n",list->size, list->name);
-		list = list->next;
 	}
 }
 
@@ -172,13 +178,13 @@ int is_files_equal(char *f1, char *f2, size_t size)
 	
 	fd1 = open(f1, O_RDWR, S_IRUSR | S_IWUSR);
 	if(fd1 <= 0) {
-		printf("Error opening file %s\n", f1);
+		printf("Unable to open file: %s\n", f1);
 		inc_files_failed();
 		return ERROR;
 	}
 	fd2 = open(f2, O_RDWR, S_IRUSR | S_IWUSR);
 	if(fd2 <= 0) {
-		printf("Error opening file %s\n", f2);
+		printf("Unable to open file: %s\n", f2);
 		close(fd1);
 		inc_files_failed();
 		return ERROR;
@@ -186,7 +192,7 @@ int is_files_equal(char *f1, char *f2, size_t size)
 
 	fm1 = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
 	if(MAP_FAILED == fm1) {
-		printf("Error mapping file %s\n", f1);
+		printf("Unable to map file: %s\n", f1);
 		close(fd1);
 		close(fd2);
 		inc_files_failed();
@@ -195,7 +201,7 @@ int is_files_equal(char *f1, char *f2, size_t size)
 		
 	fm2 = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
 	if(MAP_FAILED == fm2) {
-		printf("Error mapping file %s\n", f2);
+		printf("Unable to map file: %s\n", f2);
 		close(fd1);
 		close(fd2);
 		munmap(fm1, size);
@@ -211,18 +217,17 @@ int is_files_equal(char *f1, char *f2, size_t size)
 	return !ret;
 }
 
-int crc_calc(struct crc_entry *crc, struct list_entry *list)
+int crc_calc(struct list_entry *list)
 {
 	void *fm;
 	int fd, size;
-
-	crc->fd = 0;
-	crc->mm = NULL;
+	list->fd = 0;
+	list->mm = NULL;
 
 	fd = open(list->name, O_RDWR, S_IRUSR | S_IWUSR);
 	if(fd <= 1) {
 		printf("Unable to open file: %s\n", list->name);
-		return 1;
+		return ERR_OPEN_FILE;
 	}
 
 	size = list->size > 512 ? 512 : list->size;
@@ -230,140 +235,89 @@ int crc_calc(struct crc_entry *crc, struct list_entry *list)
 	if(MAP_FAILED == fm) {
 		printf("Unable to map file: %s\n", list->name);
 		close(fd);
-		return 1;
+		return ERR_MEM_MAP;
 	}	
 
-	crc->crc = crc16(0, fm, size);
-	crc->fd = fd;
-	crc->mm = fm;
+	list->crc = crc16(0, fm, size);
+	list->fd = fd;
+	list->mm = fm;
 	return 0;
 }
 
-int push_to_crc_list(struct crc_entry* *hash_list, struct list_entry *list)
-{
-	struct crc_entry *new_ent;
-	new_ent = malloc(sizeof(struct crc_entry));
-	if(NULL == new_ent) {
-		printf("Memory allocation error\n");
-		return 1;
-	}
-	new_ent->name = list->name;
-	new_ent->next = *hash_list;
-	if(crc_calc(new_ent, list)) {
-		free(new_ent);
-		return 0;  /*  return success  */
-	}		
-	*hash_list = new_ent;
-	return 0;
-}
-
-void do_crc_comparisons(struct crc_entry* *hl, unsigned long size)
+void do_crc_comparisons(struct list_entry *list)
 {
 	int files_eq, display_first = 0;
-	struct crc_entry *first = *hl, *list, *prev;
+	unsigned long size = list->size;
+	struct list_entry *first, *sec;
 
-	while(NULL != first && NULL != first->next) {
-		list = first->next;
-		prev = first;
-		while(list != NULL) {
-			if(first->crc != list->crc) {
-				list = list->next;
-				prev = prev->next;
+	for(first = list; NULL != first && NULL != first->next_down; first = first->next_down) {
+		if (0 == first->size)
+			continue;
+		for(sec = first->next_down; sec != NULL; sec = sec->next_down) {
+			if(0 == sec->size || first->crc != sec->crc)
 				continue;
-			}
-
-			files_eq = !memcmp(first->mm, list->mm, size);
+			files_eq = !memcmp(first->mm, sec->mm, size);
 			if(files_eq){
 				if(!display_first) {
 					printf("\n%lu\n%s\n",size, first->name);
 					display_first = 1;
 				}
-				printf("%s\n", list->name);
-/*				nxt:                */
-				prev->next = list->next; /* ??????? error if list->next == NULL */
-				munmap(list->mm, size);
-				close(list->fd);
-				free(list);
-				list = prev->next;
-			}else{
-				list = list->next;
-				prev = prev->next;
+				printf("%s\n", sec->name);
+				munmap(sec->mm, size);
+				sec->size = 0;
+				close(sec->fd);
+				sec->fd = 0;
 			}
 		}
-		
 		display_first = 0;
-		first = first->next;
 	}
 }
 
 void file_comparisons(struct list_entry *l)
 {
-	struct list_entry *sf;	/* second_file */
-	struct crc_entry **cl;	/* crc list */
-	struct crc_entry *first;
+	struct list_entry *dl;	/* down list */
 
-	if (NULL != l->next)
-		l = l->next; /* drop empty entry */
-	else
-		return;
-	
-	while(NULL != l->next) {
-		if(l->size != l->next->size) {
-			l = l->next;
+	for(;NULL != l; l = l->next) {
+		if(NULL == l->next_down) {
 			continue;
 		}
-
-		sf = l->next;
-		if((NULL != sf  &&  l->size == sf->size) &&
-			((NULL != sf->next &&  sf->next->size != sf->size) || NULL == sf->next)) {
-				if(is_files_equal(l->name, sf->name, l->size))
-					printf("\n%lu\n%s\n%s\n", l->size, l->name, sf->name);
+		if(NULL == l->next_down->next_down) {
+				if(is_files_equal(l->name, l->next_down->name, l->size))
+					printf("\n%lu\n%s\n%s\n", l->size, l->name, l->next_down->name);
 		} else {
-			/*  More than two files have the same size */
-			first = malloc(sizeof(struct crc_entry));
-			if(NULL == first) {
-				printf("Memory allocation error\n");
-				return;
-			}
-			first->name = l->name;
-			first->next = NULL;
-			crc_calc(first, l);
-			cl = &first;
-			while(NULL != sf && l->size == sf->size) {
-				if(push_to_crc_list(cl, sf)) {
-					printf("Insufficient of memory\n");
-					unmap_and_close(*cl, l->size);
-					clean_malloc((struct list_entry*) *cl);
-					return;
-				}
-				sf = sf->next;
-				l = l->next;
-			}
-			do_crc_comparisons(cl, l->size);
-
-			unmap_and_close(*cl, l->size);
-			clean_malloc((struct list_entry*) *cl);
+			/*
+			* More than two files have the same size 
+			* For this case first calculate CRC of each file part,
+			* then the files is compared if CRC is equal.
+			*/
+			for(dl = l; NULL != dl; dl = dl->next_down)
+				crc_calc(dl);
+			do_crc_comparisons(l);
+			unmap_and_close(l);
 		}
-		l = l->next;
 	}
 }
 
 int	file_dup_find(char *dir, unsigned long min_size)
 {
 	struct list_entry *list;
-	int ret;
 	list = malloc(sizeof(struct list_entry));
+	if (NULL == list) {
+		printf("Insufficient of memory\n");
+		return ERR_NOMEM;
+	}
 	list->next = NULL;
+	list->next_down = NULL;
 	list->size = 0;
 	list->name[0] = 0;
 
 	printf("Scanning files...\n");
-	ret = scan_dir(dir, min_size, insert_file_entry, list);
+	scan_dir(dir, min_size, insert_file_entry, list);
 	printf("\nStarting comparisons...\n");
 	file_comparisons(list);
 	printf("%d files scanned, %d file(s) failed.\n", files_scanned(), files_failed());
 	clean_malloc(list);
-	return ret;
+	return 0;
 }
 
 int	main(int argc, char *argv[])
